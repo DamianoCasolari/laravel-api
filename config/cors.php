b0f980b6d3cc44157b38a8b6596e6a43b4ab951a
DamianoCasolari <damianocasolari@gmail.com>
2023-08-22 22:35:54 +0200

    advice change 'alowed origins'

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('APP_FRONTEND_URL', 'https://damianocasolari.com', 'http://localhost:5174', 'http://localhost:5173')],
+    // 'allowed_origins' => [env('APP_FRONTEND_URL', 'https://damianocasolari.com', 'http://localhost:5174', 'http://localhost:5173')],
+
+    'allowed_origins' => [env('APP_FRONTEND_URL', ['https://damianocasolari.com', 'http://localhost:5174', 'http://localhost:5173'])],
+
 
     'allowed_origins_patterns' => [],
 
